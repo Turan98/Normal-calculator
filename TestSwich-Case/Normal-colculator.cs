1fea6c2a9e5929612c1d5267308ec11387c250e2
Turan98 <48137040+Turan98@users.noreply.github.com>
2022-10-03 23:32:58 -0700

    test4

---
+++ TestSwich-Case/Normal-colculator.cs
@@ -0,0 +1,35 @@
+﻿// Colculator with four function
+double a, b, c;
+string op;
+//while(true){
+Console.WriteLine("Enter first num: ");
+a = Convert.ToDouble(Console.ReadLine());
+Console.Write("Please enter an operand (+, -, /, *): ");
+op = Console.ReadLine();
+Console.WriteLine("Enter second num: ");
+b = Convert.ToDouble(Console.ReadLine());
+
+switch(op)
+{
+    case "-":
+    c = a - b;
+    break;
+    case "+":
+    c = a + b;
+    break;
+     case "/":
+    c = a / b;
+    break;
+     case "*":
+    c = a * b;
+    break;
+     default:
+     c = 0;
+     break;
+     
+}
+    Console.WriteLine("Our result is: " + c);
+    //break;
+//}
+    
+
